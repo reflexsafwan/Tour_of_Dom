b1da889682bd6063f0753032d7c8f6876d9a0ccf
reflexsafwan <reflex.safwan@gmail.com>
2023-02-13 01:42:30 +0600

    review project practised done

--- review.js
+++ review.js
@@ -1,4 +1,11 @@
 document.getElementById("review-btn").addEventListener("click", () => {
-  const element = document.getElementById("floatingTextarea2").value;
-  console.log(value);
+  let element = document.getElementById("floatingTextarea2");
+  elementValue = element.value;
+  console.log(elementValue);
+  const ulELement = document.getElementById("commnet-container");
+  const p = document.createElement("p");
+  p.innerText = elementValue;
+
+  ulELement.appendChild(p);
+  element.value = "";
 });
